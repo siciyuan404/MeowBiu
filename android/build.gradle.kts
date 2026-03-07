a97ddaea12b3c3c52b47ef8b9ce1ac73a1219373
siciyuan404 <siciyuan404@github.com>
2026-03-07 17:55:16 +0800

    fix: 添加 ffmpeg_kit namespace 兼容修复

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,6 +7,20 @@ allprojects {
     }
 }
 
+// 修复 ffmpeg_kit_flutter 等插件的 namespace 问题
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.LibraryExtension) {
+                if (android.namespace == null) {
+                    android.namespace = project.group.toString()
+                }
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
